ddf77dc94664b1323db8004ccf4b6c2b78d43fe5
Yannick <yannick.kupferschmidt@googlemail.com>
2024-04-04 01:09:54 +0300

    fix:impressum

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -4,8 +4,28 @@ export const metadata = {
 
 export default function Impressum() {
     return (
-        <>
+        <section className="relative justify-center">
+            <p>
+                Impressum<br/>
+                Trainercontainer<br/>
+                Gräfstr. 45<br/>
+                60486 Frankfurt am Main<br/>
+                <br/>
+                Kontakt<br/>
+                E-Mail: hello@cannabis.club<br/>
+                <br/>
+                EU-Streitschlichtung<br/>
+                Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:<br/>
+                https://ec.europa.eu/consumers/odr/.<br/>
+                Unsere E-Mail-Adresse finden Sie oben im Impressum.<br/>
+                <br/>
+                Verbraucherstreitbeilegung/Universalschlichtungsstelle<br/>
+                Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+                Verbraucherschlichtungsstelle teilzunehmen.<br/>
+                <br/>
+                Quelle: eRecht24
+            </p>
+        </section>
 
-        </>
     )
 }
